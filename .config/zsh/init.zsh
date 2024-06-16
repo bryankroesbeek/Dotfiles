b2eace2d4197ce68b90cc41e591a285c73abbf2e
Bryan <bryank.b96@gmail.com>
2024-06-16 11:30:04 +0200

    zsh: Add init.zsh to easily source zsh files

---
+++ .config/zsh/init.zsh
@@ -0,0 +1,3 @@
+source ~/repositories/dotfiles/config/zsh/system-config.zsh
+source ~/repositories/dotfiles/config/zsh/debian-system-config.zsh
+source ~/repositories/dotfiles/config/zsh/history.zsh
