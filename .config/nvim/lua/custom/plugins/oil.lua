89f0a1e35c540ac3fb56f671b5c494ce80bbe9df
Bryan <bryank.b96@gmail.com>
2024-11-25 07:48:40 +0000

    nvim: Open preview window when opening oil

--- .config/nvim/lua/custom/plugins/oil.lua
+++ .config/nvim/lua/custom/plugins/oil.lua
@@ -6,4 +6,17 @@ return {
   -- Optional dependencies
   dependencies = { { "echasnovski/mini.icons", opts = {} } },
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+
+  config = function()
+    local oil = require("oil")
+    local util = require("oil.util")
+
+    -- REQUIRED
+    oil:setup()
+    -- REQUIRED
+
+    util.run_after_load(0, function ()
+      oil.open_preview()
+    end)
+  end,
 }
