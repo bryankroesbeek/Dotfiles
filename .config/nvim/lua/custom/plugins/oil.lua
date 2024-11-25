63190d5a847324d71cc771b2c37386d707880f6f
Bryan <bryank.b96@gmail.com>
2024-11-25 07:48:58 +0000

    nvim: Add binding to open oil

--- .config/nvim/lua/custom/plugins/oil.lua
+++ .config/nvim/lua/custom/plugins/oil.lua
@@ -18,5 +18,19 @@ return {
     util.run_after_load(0, function ()
       oil.open_preview()
     end)
+
+    vim.keymap.set("n", "\\", function()
+      local oil_path = "oil://"
+      local path = vim.fn.expand("%")
+      local open = path:sub(1, #oil_path) == oil_path
+      if (open) then
+        oil.close()
+      else
+        oil.open()
+        util.run_after_load(0, function ()
+          oil.open_preview()
+        end)
+      end
+    end)
   end,
 }
