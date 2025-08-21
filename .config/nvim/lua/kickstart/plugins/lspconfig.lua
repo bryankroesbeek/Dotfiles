da5f2c71d35f8a026c69c3366b34890e1c98bde4
Bryan <bryank.b96@gmail.com>
2025-08-21 14:50:01 +0000

    nvim: Enable inline hints

--- .config/nvim/lua/kickstart/plugins/lspconfig.lua
+++ .config/nvim/lua/kickstart/plugins/lspconfig.lua
@@ -131,6 +131,9 @@ return {
           -- code, if the language server you are using supports them
           --
           -- This may be unwanted, since they displace some of your code
+          vim.diagnostic.config {
+            virtual_text = true,
+          }
           if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
             map('<leader>th', function()
               vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
