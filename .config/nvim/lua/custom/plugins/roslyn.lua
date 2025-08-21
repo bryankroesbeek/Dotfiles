5e676bbdc78f99627c14d1104366c19cbc29e989
Bryan <bryank.b96@gmail.com>
2025-08-21 14:49:41 +0000

    nvim: Add Roslyn lsp

---
+++ .config/nvim/lua/custom/plugins/roslyn.lua
@@ -0,0 +1,10 @@
+return {
+  "seblyng/roslyn.nvim",
+  ---@module 'roslyn.config'
+  ---@type RoslynNvimConfig
+  opts = {
+    -- your configuration comes here; leave empty for default settings
+    broad_search = true,
+    disable_lsp_setup = true,
+  },
+}
