3fb8e3488ebf34abef001123b0e43f6f604c007a
Bryan <bryank.b96@gmail.com>
2025-11-18 13:07:43 +0000

    fish: Add TRUECOLOR variable

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,6 +6,8 @@ function fish_prompt
     echo ($HOME/repos/emerald-prompt/bin/emerald fish)
 end
 
+set -gx COLORTERM truecolor
+
 fish_add_path -a $HOME/.local/bin
 fish_add_path -a $HOME/.local/scripts
 fish_add_path -a $HOME/.dotnet/tools
