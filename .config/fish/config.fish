57c0b28355e5476f87859a278241cb43a8ef46b9
Bryan <bryank.b96@gmail.com>
2025-11-18 13:06:31 +0000

    fish: Add to path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,3 +6,6 @@ function fish_prompt
     echo ($HOME/repos/emerald-prompt/bin/emerald fish)
 end
 
+fish_add_path -a $HOME/.local/bin
+fish_add_path -a $HOME/.local/scripts
+fish_add_path -a $HOME/.dotnet/tools
