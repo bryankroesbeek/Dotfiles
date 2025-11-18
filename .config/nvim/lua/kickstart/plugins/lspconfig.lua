eee7c80655ca3dc4ed0431541879b36fa0d47893
Bryan <bryank.b96@gmail.com>
2025-11-18 13:08:37 +0000

    nvim: Update underline rendering level

--- .config/nvim/lua/kickstart/plugins/lspconfig.lua
+++ .config/nvim/lua/kickstart/plugins/lspconfig.lua
@@ -175,7 +175,9 @@ return {
       vim.diagnostic.config {
         severity_sort = true,
         float = { border = 'rounded', source = 'if_many' },
-        underline = { severity = vim.diagnostic.severity.INFO },
+        underline = { severity = {
+          min = vim.diagnostic.severity.HINT,
+        } },
         signs = vim.g.have_nerd_font and {
           text = {
             [vim.diagnostic.severity.ERROR] = '󰅚 ',
